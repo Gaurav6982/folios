3cf14c7d5e50dcb61a6316effd49b421c107001d
akansha-singh <singhakansha281@gmail.com>
2020-10-02 12:17:24 +0530

    pwa

--- resources/js/components/Footer.js
+++ resources/js/components/Footer.js
@@ -6,11 +6,12 @@ export class Footer extends Component {
             <div className="container"><br/>
                 <div className="row">
                     <div className="mx-auto">
-                        <a href=""><img src="assets/images/Mask Group.png" style={{width:'25px',height:'25px',margin:'5px'}}/></a>
-                        <a href=""><img src="assets/images/Mask Group(1).svg" style={{width:'25px',height:'25px',margin:'5px'}}/></a>
-                        <a href=""><img src="assets/images/Mask Group(2).svg" style={{width:'25px',height:'25px',margin:'5px'}}/></a>
-                        <a href=""><img src="assets/images/Mask Group(3).svg" style={{width:'25px',height:'25px',margin:'5px'}}/></a>
-                        <a href=""><img src="assets/images/Mask Group(4).svg" style={{width:'25px',height:'25px',margin:'5px'}}/></a>
+                        <a href="https://www.linkedin.com/company/dsc-jssaten"><img src="assets/images/Mask Group.png" style={{width:'25px',height:'25px',margin:'5px'}}/></a>
+                        <a href="https://instagram.com/dscjssaten?igshid=9jnfro1wptxw"><img src="assets/images/Mask Group(1).svg" style={{width:'25px',height:'25px',margin:'5px'}}/></a>
+                        <a href="https://www.facebook.com/dscjssnoida/"><img src="assets/images/f.svg" style={{width:'25px',height:'25px',margin:'5px'}}/></a>
+                        <a href="https://github.com/DSC-JSS-NOIDA"><img src="assets/images/Mask Group(3).svg" style={{width:'25px',height:'25px',margin:'5px'}}/></a>
+                        <a href="https://www.google.com/url?sa=t&source=web&rct=j&url=https://m.youtube.com/channel/UCkELk5JFDceaSf8pBa19kDQ&ved=2ahUKEwjCqJuhmJTsAhVQ6nMBHayRAssQjjgwAHoECAEQAQ&usg=AOvVaw0m_sGZWcJOlhOF640lN-as"><img src="assets/images/yt.svg" style={{width:'25px',height:'25px',margin:'5px'}}/></a>
+                        <a href="https://mobile.twitter.com/dscjssaten"><img src="assets/images/twitter.svg" style={{width:'25px',height:'25px',margin:'5px'}}/></a>
                     </div>
                 </div>
                 <div className="row">
