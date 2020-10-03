b6c45cd0bff561115adb67eaf4088fe33e27e457
akansha-singh <singhakansha281@gmail.com>
2020-10-03 14:30:48 +0530

    updates

--- resources/js/components/HeaderComponent2.js
+++ resources/js/components/HeaderComponent2.js
@@ -4,21 +4,38 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 export class HeaderComponent2 extends Component {
 
+    constructor(props){
+        super(props);
+    
+        this.state = {
+          Url: ''
+        };
+      }
+    
+    async componentWillMount() {
+        const url = "/get-slug";
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({Url: `${window.location.hostname}/portfolio/${data.key}`});
+      }
+    
     render() {
-        
         return (
             <BrowserRouter>
             <div>
                 <Navbar className="fixed-top navbar" color="white" light expand="lg">
                     <div className="container">
                         <NavbarBrand className="mr-auto" href="/">
-                            <img  src="assets/images/logoSmall.svg"  height="29px"  width="153px" alt=""
+                            <img src="assets/images/logoSmall.svg" height="29px" width="153px" alt=""
                         />
                         </NavbarBrand>
                         <Nav navbar className="ml-auto" style={{float:'right',color:'#000000'}}>
-                        <NavItem className="nav-items">
-                            <button className="btn btn-primary" >Logout</button>
-                        </NavItem>
+                            <NavItem className="nav-items">
+                                <NavLink color="dark" className="nav-link" to={this.state.Url}>Profile</NavLink>
+                            </NavItem>
+                            <NavItem className="nav-items">
+                                <Button className="btn btn-primary" >Logout</Button>
+                            </NavItem>
                         </Nav>
                     </div>
                 </Navbar>
--- resources/js/components/Intro.js
+++ resources/js/components/Intro.js
@@ -1,23 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import SignUp from './SignUp';
 import {Link} from 'react-router-dom';
 
 export class PageOne extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isSignUpModalOpen: false
+        };
+        this.toggleModalSignUp = this.toggleModalSignUp.bind(this);
+
+    }
+    toggleModalSignUp(){
+        this.setState({
+            isSignUpModalOpen: !this.state.isSignUpModalOpen
+        });
+    }
     render() {
         return (
+            <div>
             <div className="container" >
                 <div className="row">
-                    <div className="col-lg-5 homecontent" style={{marginTop:'70px',paddingRight:'30px'}}>
+                    <div className="col-lg-5 homecontent" style={{marginTop:'50px',paddingRight:'30px'}}>
                         <h2 className="heading_one">Bring all of your links<br/> in one place</h2><br/>
                         <p>Keep all your important links on your fingure tips and such easy sharing is never possible before. An ideal place to keep and share your presence.</p>
                         <br/>
-                        <button type="button" className="btn buttons" style={{width:'200px', height:'auto',color:'#ffffff',fontSize:'15px'}} >Sign Up for FREE</button>
+                        <button type="button" onClick={this.toggleModalSignUp} className="btn buttons btn-round btn-sm" style={{color:'#ffffff',fontSize:'15px'}} >Sign Up for FREE</button>
                         <br/><br/>
-                        <img src="assets/images/img2.png" width="170px" height="35px" alt="img"/>
+                        <img src="assets/images/img2.png" width="228px" height="26px" alt="img"/>
                     </div>
                     <div className="col-lg-7">
                         <center><img className="homepage-img" src="assets/images/img1.svg" style={{textAlign:'center', maxWidth:'100%', maxHeight:'100%'}} width="auto" alt="img"/></center>
                     </div>
                 </div>
+            </div>
+            <Modal isOpen={this.state.isSignUpModalOpen} toggle={this.toggleModalSignUp}>
+                <ModalHeader toggle={this.toggleModalSignUp}>Sign Up</ModalHeader>
+                <ModalBody>
+                    <br/>
+                    <SignUp/>
+                </ModalBody>
+            </Modal>
             </div>
         )
     }
